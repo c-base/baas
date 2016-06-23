589ad4455d88c52802ba3d403a7c72298899ab68
Uwe Kamper <me@uwekamper.de>
2016-06-24 00:47:39 +0200

    Initial commit

---
+++ baas.sh
@@ -0,0 +1,54 @@
+#/bin/bash
+while [ /bin/true ]
+do
+mosquitto_pub -h c-beam.cbrp3.c-base.org -t 'c_out/julia' -m 'smart data'
+sleep 120
+mosquitto_pub -h c-beam.cbrp3.c-base.org -t 'c_out/julia' -m 'didschitell trensformäischen'
+sleep 120
+mosquitto_pub -h c-beam.cbrp3.c-base.org -t 'c_out/julia' -m 'mobeil gehddon'
+sleep 120
+mosquitto_pub -h c-beam.cbrp3.c-base.org -t 'c_out/julia' -m 'artifischel everything'
+sleep 120
+mosquitto_pub -h c-beam.cbrp3.c-base.org -t 'c_out/julia' -m 'embient ju ex'
+sleep 120
+mosquitto_pub -h c-beam.cbrp3.c-base.org -t 'c_out/julia' -m 'quantum advertisement'
+sleep 120
+mosquitto_pub -h c-beam.cbrp3.c-base.org -t 'c_out/julia' -m 'disrapptiff computing'
+sleep 120
+mosquitto_pub -h c-beam.cbrp3.c-base.org -t 'c_out/julia' -m 'for die printing'
+sleep 120
+mosquitto_pub -h c-beam.cbrp3.c-base.org -t 'c_out/julia' -m 'analytics 4.0'
+sleep 120
+mosquitto_pub -h c-beam.cbrp3.c-base.org -t 'c_out/julia' -m 'ahead of time deployment'
+sleep 120
+mosquitto_pub -h c-beam.cbrp3.c-base.org -t 'c_out/julia' -m 'blocktschäin banking'
+sleep 120
+mosquitto_pub -h c-beam.cbrp3.c-base.org -t 'c_out/julia' -m 'next dscheneräischen platform'
+sleep 120
+mosquitto_pub -h c-beam.cbrp3.c-base.org -t 'c_out/julia' -m 'heibritt mainframe'
+sleep 120
+mosquitto_pub -h c-beam.cbrp3.c-base.org -t 'c_out/julia' -m 'cloud diepleuhments'
+sleep 120
+mosquitto_pub -h c-beam.cbrp3.c-base.org -t 'c_out/julia' -m 'Ädscheil management'
+sleep 120
+mosquitto_pub -h c-beam.cbrp3.c-base.org -t 'c_out/julia' -m 'sörwerleß computing'
+sleep 120
+mosquitto_pub -h c-beam.cbrp3.c-base.org -t 'c_out/julia' -m 'cyber offenz'
+sleep 120
+mosquitto_pub -h c-beam.cbrp3.c-base.org -t 'c_out/julia' -m 'enterprise junikörnells'
+sleep 120
+mosquitto_pub -h c-beam.cbrp3.c-base.org -t 'c_out/julia' -m 'enterprise environments'
+sleep 120
+mosquitto_pub -h c-beam.cbrp3.c-base.org -t 'c_out/julia' -m 'mobeil first'
+sleep 120
+mosquitto_pub -h c-beam.cbrp3.c-base.org -t 'c_out/julia' -m 'internet of everything'
+sleep 120
+mosquitto_pub -h c-beam.cbrp3.c-base.org -t 'c_out/julia' -m 'nätiv advertising'
+sleep 120
+mosquitto_pub -h c-beam.cbrp3.c-base.org -t 'c_out/julia' -m 'growth hacking'
+sleep 120
+mosquitto_pub -h c-beam.cbrp3.c-base.org -t 'c_out/julia' -m 'omnitschännel marketing'
+sleep 120
+mosquitto_pub -h c-beam.cbrp3.c-base.org -t 'c_out/julia' -m 'cyber defense'
+sleep 120
+done
